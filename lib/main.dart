feb664873d8715e519428a0b56d578f4e4380fad
AhmadAbbas8 <ahmadabbass822@gmail.com>
2024-06-29 21:52:03 +0300

    some edits

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       locale: Locale('en'),
-      home: const HomeCustomerScreen(),
+      home: const LoginScreen(),
     );
   }
 }
